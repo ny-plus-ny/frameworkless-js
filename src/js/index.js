ed0d3ee905c60f2e4a0afbb526e954aae034fb05
ny-plus-ny <ny.plus.ny@gmail.com>
2023-11-20 13:33:00 +0900

    fix: url id값 추출하는 정규표현식 추가

--- src/js/index.js
+++ src/js/index.js
@@ -1,30 +1,58 @@
 import homePage from './home.js';
 import postPage from './post.js';
+import designPage from './design.js';
 import notFound from './404.js';
 
 const HEADER = document.getElementById("header")
 const MAIN = document.querySelector("main")
-// const pathToRegex = path => new RegExp("^" + path.replace(/\//g, "\\/").replace(/:\w+/g, "(.+)") + "$")
 
 const routes = [
-    {path: "/", component: homePage },
-    {path: "/post:id", component: postPage },
-    // {path: "/design", component: designPage },
-    // {path: "/desgin:id", component: designPage },
+  {path: "/", component: homePage },
+  {path: "/post/:id", component: postPage },
+  {path: "/design", component: designPage },
 ]
 
-console.log("index3.js ok")
+const pathToRegex = path => new RegExp("^" + path.replace(/\//g, "\\/").replace(/:\w+/g, "(.+)") + "$")
 
-const router = async path => {
-    const _path = path ?? window.location.pathname;
-    console.log("_path is: ",_path)
-    try {
-      const component = routes.find(route => route.path === _path)?.component || notFound;
-      MAIN.replaceChildren(await component());
+const router = async () => {
+    const _path = window.location.pathname;
+    const matchedRoute = routes.map(route => {
+      return _path.match(pathToRegex(route.path))
+    })
+    const notNullRoute =  matchedRoute.find( route => route !== null) 
+    const componentFunc = (dataId) => {
+      if(dataId) {
+        routes.find(route => route.path === notNullRoute[0])?.component(dataId) || notFound()
+      } else {
+        routes.find(route => route.path === notNullRoute[0])?.component() || notFound() 
+      }
+    };
+    // const render = routes.find(route => route.path === location.pathname)?.component || notFound
+    const showComponent = async (id) => {
+      if (id){
+        console.log("id 있음 ",id)
+        MAIN.replaceChildren(await componentFunc(id));
+      } else {
+        console.log("id 없음")
+        MAIN.replaceChildren(await componentFunc());
+      }
+    }
+
+  try {
+      console.log("notNullRoute:", notNullRoute)
+      // const component = routes.find(route => route.path === notNullRoute[0])?.component || notFound;      
+      // MAIN.replaceChildren(await showComponent(notNullRoute[1]));
+      if (typeof(notNullRoute[1]) === 'string') {
+        showComponent(notNullRoute[1])
+      } else {
+        showComponent()
+      }
     } catch (err) {
       console.error(err);
     }
   };
+
+
 
 HEADER.addEventListener("click", (e)=>{
     if (!e.target.matches(" header > a ")){
@@ -32,19 +60,18 @@ HEADER.addEventListener("click", (e)=>{
     }
     e.preventDefault();
     const pathHref = e.target.getAttribute('href');
-
     if (window.location.pathname === pathHref) return;
-
     window.history.pushState(null, null, pathHref);
-
-    router(pathHref)
+    router()
 })
+
 
 
 window.addEventListener('popstate', () => {
     router();
   });
 
-window.addEventListener('DOMContentLoaded', ()=> {router()});
+window.addEventListener('DOMContentLoaded', ()=> {router()
+});
 
 
